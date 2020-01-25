ef1b606390bd9a11193d4483dfaf0536a43a0b36
acviex <first-june@yandex.ru>
2020-01-26 01:56:47 +0300

    Implement second page

--- js/core/core.js
+++ js/core/core.js
@@ -1,6 +1,9 @@
 "use strict"
 
 $("#main-button").click(function() {
-   $("#m-window").css("background-size", "200% 25%")
-   $("#m-window").css("animation", "gradient-mini 15s ease infinite")
+   $("#m-window").css("background-size", "200% 25%");
+   $("#m-window").css("animation", "gradient-mini 15s ease infinite");
+   $("#m-window").addClass("second-page");
+   $("#m-window h2").text("Enter Your Credentials");
+   $("#m-window button").text("NEXT");
 });
