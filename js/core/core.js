09cd52adcd9120fbd92b22c350f2d3aebb7d53c6
acviex <first-june@yandex.ru>
2020-01-25 19:23:57 +0300

    intro

---
+++ js/core/core.js
@@ -0,0 +1,6 @@
+"use strict"
+
+$("#main-button").click(function() {
+   $("#m-window").css("background-size", "200% 25%")
+   $("#m-window").css("animation", "gradient-mini 15s ease infinite")
+});
