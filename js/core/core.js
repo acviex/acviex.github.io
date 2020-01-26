13f4699038c7aebed5f8614ce7b201412841f5c8
acviex <first-june@yandex.ru>
2020-01-26 20:46:25 +0300

    Second page combo box

--- js/core/core.js
+++ js/core/core.js
@@ -4,6 +4,6 @@ $("#main-button").click(function() {
    $("#m-window").css("background-size", "200% 25%");
    $("#m-window").css("animation", "gradient-mini 15s ease infinite");
    $("#m-window").addClass("second-page");
-   $("#m-window h2").text("Enter Your Credentials");
-   $("#m-window button").text("NEXT");
+   $("#m-window h2").text("Authentication");
+   $("#main-button").text("NEXT");
 });
