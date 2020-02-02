4fe480c5a8f37defe4f1460e978b0def23713872
acviex <first-june@yandex.ru>
2020-02-02 18:10:01 +0300

    Chat page

--- js/core/core.js
+++ js/core/core.js
@@ -1,9 +1,17 @@
 "use strict"
 
 $("#main-button").click(function() {
-   $("#m-window").css("background-size", "200% 25%");
-   $("#m-window").css("animation", "gradient-mini 15s ease infinite");
-   $("#m-window").addClass("second-page");
-   $("#m-window h2").text("Authentication");
-   $("#main-button").text("NEXT");
+   if ($("#main-button").text() === "START MESSAGING")
+   {
+      $("#m-window").css("background-size", "200% 25%");
+      $("#m-window").css("animation", "gradient-mini 15s ease infinite");
+      $("#m-window").addClass("second-page");
+      $("#m-window h2").text("Authentication");
+      $("#main-button").text("NEXT");
+   }
+   else
+   {
+      $("#m-window").remove();
+      $(".chat").addClass("visible");
+   }
 });
